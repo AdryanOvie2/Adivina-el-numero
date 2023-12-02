6dc845bae686b03cc2a53fcdd340ee83c5705d36
AdryanOvie2 <adryanovie2@gmail.com>
2023-12-02 22:32:54 +0100

    finished

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -2,11 +2,24 @@
 # diremos si es mayor | menor | igual al creado por la PC
 import random
 
+
 def adivina_el_num(num):
     print("==================================")
     print("Bienvenido al juego!")
     print("==================================")
     print("Tu objetivo es adivinar el número generado por la IA.")
     
-    numero_aletorio = random.randint()
-    
+    numero_aletorio = random.randint(1, num) # num aleatorio entre 1 y num
+
+    prediccion = 0
+
+    while prediccion != numero_aletorio:
+        prediccion = int(input(f"Adivina un número entre 1 y {num}: "))
+        if prediccion < numero_aletorio:
+            print("Intenta otra vez. Este número es pequeño.")
+        elif prediccion > numero_aletorio:
+            print("Intenta otra vez. Este número es muy grande.")
+    print(f"Felicitaciones! Adivinaste el número {num} correctamente!")
+
+
+adivina_el_num(10)
